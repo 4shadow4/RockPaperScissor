07f49d8b6b0024c5ad62ab8fd0444d036b31b407
JohannH <johann.hertz2009@gmail.com>
2025-11-08 00:38:27 -0500

    created UI for Rock Paper Scissor

--- script.js
+++ script.js
@@ -1,75 +1,11 @@
-/*
-    #Rock Paper Sisor
 
-    ##Pseudocode:
+const buttons = document.querySelectorAll("button");
 
-    INIT getComChoice function
+const output = document.querySelector("#results");
 
-    getComChoice function:
----
-    SET randomNumber to a random Number between 0 and 2 with Math.floor and Math.random
-    INIT choice arraz wich contains three string elements: Rock, Paper, Scissor
-    RETURN the choice[randomNumber] element
----
-
-    INIT getUserChoice function
-
-    getUserChoice function:
----
-    SET userChoice to promt("Input your Choice!")
-
-    RETURN userChoice.toLowerCase()
----
-
-    INIT decideWinner function with two Parameters: choice1, choice2
-
-    decideWinner(humanChoice,comChoice) function:
----
-    IF humanChoice === "scissor" && comChoice === "paper" THEN
-        OUTPUT You win! humanChoice beats comChoice.
-
-    ELSE IF humanChoice === "paper" && comChoice === "rock" THEN
-        OUTPUT You win! humanChoice beats comChoice.
-
-    ELSE IF humanChoice ===  comChoice  THEN
-        OUTPUT It is a draw! humanChoice is equal to comChoice. Yout have to play again.
-        CALL playRound function
-
-
-    ELSE
-        OUTPUT You lose! humanChoice looses against comChoice.  
-    ENDIF
-
----
-
-
-
-    INIT playRound function:
----
-
-    DECLAIRE variable userChoice and SET to OUTPUT of the function getUserChoice
-    DECLAIRE variable comChoice and SET to OUTPUT of the function getComChoice
-
-    CALL decideWinner function with (userChoice, comChoice)
-
----
-
-
-    INIT playGame function:
----
-    FOR i equal 0, i has to be smaler than 5, one added to i
-        CALL playRound function
-    ENDFOR
----
-
-
-    CALL playGame function
-
-
-*/
-
-let userScore;
-let comScore;
+let userScore = 0;
+let comScore = 0;
+let round = 0;
 
 
 
@@ -82,49 +18,57 @@ function getComChoice(){
 
 }
 
-function getUserChoice(){
 
-    let userChoice = prompt("Input your Choice!");
-
-    return userChoice.toLowerCase();
-}
 
 function decideWinner(humanChoice,comChoice){
 
-    (humanChoice === "scissor" && comChoice === "paper")? (console.log(`You win! ${humanChoice} beats ${comChoice}.`),userScore++):
-    (humanChoice === "paper" && comChoice === "rock")? (console.log(`You win! ${humanChoice} beats ${comChoice}.`),userScore++):
-    (humanChoice === "rock" && comChoice === "scissor")? (console.log(`You win! ${humanChoice} beats ${comChoice}.`),userScore++):
-    (humanChoice === comChoice)? (console.log(`It's a draw! ${humanChoice} is equal to ${comChoice}. You have to play again.`),playRound()):
-    (console.log(`You lose! ${comChoice} beats ${humanChoice}.`),comScore++);
     
-}
 
-function playRound(){
+    (humanChoice === "scissor" && comChoice === "paper")? (output.textContent = `You win! ${humanChoice} beats ${comChoice}.`,userScore++,round++):
+    (humanChoice === "paper" && comChoice === "rock")? (output.textContent = `You win! ${humanChoice} beats ${comChoice}.`,userScore++,round++):
+    (humanChoice === "rock" && comChoice === "scissor")? (output.textContent = `You win! ${humanChoice} beats ${comChoice}.`,userScore++,round++):
+    (humanChoice === comChoice)? (output.textContent = `It's a draw! ${humanChoice} is equal to ${comChoice}. You have to play again.`):
+    (output.textContent = `You lose! ${comChoice} beats ${humanChoice}.`,comScore++,round++);
+    
+    const span = document.createElement("span");
 
-    let userChoice = getUserChoice();
-    let comChoice = getComChoice();
+    if(round > 4){
+        if(userScore > comScore){
 
-    decideWinner(userChoice, comChoice);
-}
+            span.textContent = `You won the 5 match game with ${userScore} to ${comScore}.`;
 
+        } else{
+            span.textContent = `You lost the 5 match game with ${userScore} to ${comScore}.`;
+        }
+        output.appendChild(document.createElement("br"))
+        output.appendChild(span);
 
-function playGame(){
-
-    userScore = 0;
-    comScore = 0;
-
-
-    for(let i = 0; i < 5; i++){
-        playRound();
-    }
-
-    if(userScore > comScore){
-
-        console.log(`You won the 5 match game with ${userScore} to ${comScore}.`)
-
-    } else{
-        console.log(`You lost the 5 match game with ${userScore} to ${comScore}.`)
+        round = 0;
+        userScore = 0;
+        comScore = 0;
     }
 }
 
-playGame();
+buttons.forEach(button => {
+    button.addEventListener('click',() => {
+
+
+        let userChoice = button.id;
+        let comChoice = getComChoice();
+        decideWinner(userChoice, comChoice);
+        
+    });
+});
+
+ 
+
+    
+        
+
+
+
+
+
+    
+
+
